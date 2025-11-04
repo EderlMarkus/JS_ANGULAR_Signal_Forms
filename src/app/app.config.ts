f849e304ae6bc36006970cd3c55fe6a54ac78b45
Markus Ederl <markus.ederl@r-software.at>
2025-11-04 15:33:47 +0100

    added matformfield

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideBrowserGlobalErrorListeners()
   ]
 };
