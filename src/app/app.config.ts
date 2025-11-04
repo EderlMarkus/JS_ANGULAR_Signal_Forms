88d6fec238581ff8dc4404128d7f1b7ced7cc9c3
Markus Ederl <markus.ederl@r-software.at>
2025-11-04 15:34:29 +0100

    removed router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideBrowserGlobalErrorListeners()
   ]
 };
