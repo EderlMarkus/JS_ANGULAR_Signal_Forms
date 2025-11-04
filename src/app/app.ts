b890b325825132069221fc00c02b40374cc77963
Markus Ederl <markus.ederl@r-software.at>
2025-11-04 15:31:34 +0100

    added simple_control_with_validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { Control, form, required } from '@angular/forms/signals';
+import { Control, form, maxLength, minLength, required } from '@angular/forms/signals';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { User } from '../data/users';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [Control, MatInputModule, MatButtonModule],
+  imports: [Control, MatInputModule, MatButtonModule, MatFormFieldModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -83,7 +84,9 @@ export class App {
   });
 
   protected readonly userForm = form(this.userSig, (path) => {
-    required(path.firstName);
+    required(path.firstName, { message: "Firstname is required." }),
+      minLength(path.firstName, 5, { message: "Firstname must have at least 3 Characters." }),
+      maxLength(path.firstName, 30, { message: "Firstname must not have more than 30 Characters." })
   });
 
   protected saveProposal() {
