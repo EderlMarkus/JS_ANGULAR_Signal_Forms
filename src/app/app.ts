168b76c20809a7ecb9a7fcfbef6803e194994d86
Markus Ederl <markus.ederl@r-software.at>
2025-11-12 16:06:47 +0100

    removed unneccesary comma

--- src/app/app.ts
+++ src/app/app.ts
@@ -22,9 +22,9 @@ export class App {
   });
 
   protected readonly userForm = form(this.userSig, (path) => {
-    required(path.firstName, { message: "Firstname is required." }),
-      minLength(path.firstName, 3, { message: "Firstname must have at least 3 Characters." }),
-      maxLength(path.firstName, 30, { message: "Firstname must not have more than 30 Characters." })
+    required(path.firstName, { message: "Firstname is required." });
+    minLength(path.firstName, 3, { message: "Firstname must have at least 3 Characters." });
+    maxLength(path.firstName, 30, { message: "Firstname must not have more than 30 Characters." })
   });
 
   protected saveProposal() {
