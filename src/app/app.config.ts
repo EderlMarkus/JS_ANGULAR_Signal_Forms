e231cb2d9afc9bb5f9428abc745d9f799755991d
Markus Ederl <markus.ederl@r-software.at>
2025-11-12 13:40:42 +0100

    added router because why not

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { App } from './app';
 
+
+const routes = [
+  {
+    path: "",
+    component: App
+  }
+]
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
   ]
 };
