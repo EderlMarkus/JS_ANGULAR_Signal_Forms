5601253dcaa0b7ee1ec559c698edb5fbcaa7d0d4
Markus Ederl <markus.ederl@r-software.at>
2025-12-17 13:26:31 +0100

    added multifield & treevalidation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { apply, applyWhenValue, email, Field, form, MAX_LENGTH, maxLength, MIN_LENGTH, minLength, required, schema, Schema } from '@angular/forms/signals';
+import { apply, applyWhen, applyWhenValue, email, Field, form, MAX_LENGTH, maxLength, MIN_LENGTH, minLength, required, schema, Schema, SchemaPath, validateTree } from '@angular/forms/signals';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { User } from '../data/users';
@@ -14,10 +14,31 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 export class App {
 
   private readonly _textSchema: Schema<string> = schema((fieldPath) => {
-    required(fieldPath, { message: 'This Field is required' });
     minLength(fieldPath, 3, { message: (ctx) => `Enter minimum ${ctx.field().minLength?.()} Characters` });
     maxLength(fieldPath, 50, { message: (ctx) => `Enter maximum ${ctx.field().maxLength?.()} Characters` });
   });
+
+  private readonly _requiredSchema: Schema<string> = schema((fieldPath) => {
+    required(fieldPath, { message: (ctx) => `${ctx.field().keyInParent()} is required.` });
+  })
+
+  private readonly _validateFirstNameLastNameSame = (schema: SchemaPath<User>) => {
+    validateTree(schema, (ctx) => {
+      const firstName = ctx.field.firstName().value();
+      const lastName = ctx.field.lastName().value();
+
+      if (firstName === lastName) {
+        return {
+          kind: "same_names",
+          message: "Firstname and Lastname must not be same",
+          field: ctx.field.firstName,
+          firstName,
+          lastName
+        }
+      }
+      return null;
+    })
+  }
 
   protected readonly userSig = signal<User>({
     "firstName": "Markus",
@@ -31,14 +52,36 @@ export class App {
     apply(path.firstName, this._textSchema);
     apply(path.lastName, this._textSchema);
 
-    email(path.email, { message: "Must be E-Mail" })
-    applyWhenValue(path, (ctx) => !!ctx.firstName, (path) => {
-      required(path.lastName, { message: "Lastname is required." });
+    email(path.email, { message: "Must be E-Mail" });
+
+    //OPTION 1
+    required(path.firstName, {
+      when: (ctx) => !!ctx.valueOf(path.lastName),
+      message: "Firstname is required"
     });
 
-    applyWhenValue(path, (ctx) => !!ctx.lastName, (path) => {
-      required(path.firstName, { message: "Firstname is required." });
-    });
+    required(path.lastName, {
+      when: (ctx) => !!ctx.valueOf(path.firstName),
+      message: "LastName is required"
+    })
+
+    //OPTION 2
+    // applyWhenValue(path, (ctx) => !!ctx.firstName, (path) => {
+    //   required(path.lastName, { message: "Lastname is required." });
+    // });
+
+    // applyWhenValue(path, (ctx) => !!ctx.lastName, (path) => {
+    //   required(path.firstName, { message: "Firstname is required." });
+    // });
+
+    //OPTION 3
+    // applyWhen(path, (ctx) => !!ctx.valueOf(path.firstName), (path) => apply(path.lastName, this._requiredSchema));
+    // applyWhen(path, (ctx) => !!ctx.valueOf(path.lastName), (path) => apply(path.firstName, this._requiredSchema));
+
+
+    //Multi-Field; Tree Valdiators
+    this._validateFirstNameLastNameSame(path);
+
 
   });
 
