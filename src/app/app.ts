4569038c5f0d250fcd5585bff30152b91872805a
Markus Ederl <markus.ederl@r-software.at>
2026-01-14 10:52:44 +0100

    changed to formfield

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { apply, applyWhen, applyWhenValue, email, Field, form, MAX_LENGTH, maxLength, MIN_LENGTH, minLength, required, schema, Schema, SchemaPath, validateTree } from '@angular/forms/signals';
+import { apply, applyWhen, applyWhenValue, email, FormField, form, MAX_LENGTH, maxLength, MIN_LENGTH, minLength, required, schema, Schema, SchemaPath, validateTree } from '@angular/forms/signals';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { User } from '../data/users';
@@ -7,31 +7,31 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [Field, MatInputModule, MatButtonModule, MatFormFieldModule],
+  imports: [FormField, MatInputModule, MatButtonModule, MatFormFieldModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
 
   private readonly _textSchema: Schema<string> = schema((fieldPath) => {
-    minLength(fieldPath, 3, { message: (ctx) => `Enter minimum ${ctx.field().minLength?.()} Characters` });
-    maxLength(fieldPath, 50, { message: (ctx) => `Enter maximum ${ctx.field().maxLength?.()} Characters` });
+    minLength(fieldPath, 3, { message: (ctx) => `Enter minimum ${ctx.fieldTree().minLength?.()} Characters` });
+    maxLength(fieldPath, 50, { message: (ctx) => `Enter maximum ${ctx.fieldTree().maxLength?.()} Characters` });
   });
 
   private readonly _requiredSchema: Schema<string> = schema((fieldPath) => {
-    required(fieldPath, { message: (ctx) => `${ctx.field().keyInParent()} is required.` });
+    required(fieldPath, { message: (ctx) => `${ctx.fieldTree().keyInParent()} is required.` });
   })
 
   private readonly _validateFirstNameLastNameSame = (schema: SchemaPath<User>) => {
     validateTree(schema, (ctx) => {
-      const firstName = ctx.field.firstName().value();
-      const lastName = ctx.field.lastName().value();
+      const firstName = ctx.fieldTree.firstName().value();
+      const lastName = ctx.fieldTree.lastName().value();
       if (!firstName || !lastName) return null;
       if (firstName === lastName) {
         return {
           kind: "same_names",
           message: "Firstname and Lastname must not be same",
-          field: ctx.field.firstName,
+          field: ctx.fieldTree.firstName,
           firstName,
           lastName
         }
