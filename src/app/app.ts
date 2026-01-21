fdf8fbe61bf50e8761157fa8cdcb26788f6a175c
Markus Ederl <markus.ederl@r-software.at>
2026-01-21 15:12:17 +0100

    added snackbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,96 +1,46 @@
 import { Component, inject, signal } from '@angular/core';
-import { apply, applyWhen, applyWhenValue, email, FormField, form, MAX_LENGTH, maxLength, MIN_LENGTH, minLength, required, schema, Schema, SchemaPath, submit, validateTree } from '@angular/forms/signals';
+import { FormField, form, maxLength, minLength, provideSignalFormsConfig, required, submit } from '@angular/forms/signals';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { User, Users } from '../data/users';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { firstValueFrom } from 'rxjs';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [FormField, MatInputModule, MatButtonModule, MatFormFieldModule],
+  imports: [FormField, MatInputModule, MatButtonModule, MatFormFieldModule, MatSnackBarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
 
   private userService = inject(Users);
-
-  private readonly _textSchema: Schema<string> = schema((fieldPath) => {
-    minLength(fieldPath, 3, { message: (ctx) => `Enter minimum ${ctx.fieldTree().minLength?.()} Characters` });
-    maxLength(fieldPath, 50, { message: (ctx) => `Enter maximum ${ctx.fieldTree().maxLength?.()} Characters` });
-  });
-
-  private readonly _requiredSchema: Schema<string> = schema((fieldPath) => {
-    required(fieldPath, { message: (ctx) => `${ctx.fieldTree().keyInParent()} is required.` });
-  })
-
-  private readonly _validateFirstNameLastNameSame = (schema: SchemaPath<User>) => {
-    validateTree(schema, (ctx) => {
-      const firstName = ctx.fieldTree.firstName().value();
-      const lastName = ctx.fieldTree.lastName().value();
-      if (!firstName || !lastName) return null;
-      if (firstName === lastName) {
-        return {
-          kind: "same_names",
-          message: "Firstname and Lastname must not be same",
-          field: ctx.fieldTree.firstName,
-          firstName,
-          lastName
-        }
-      }
-      return null;
-    })
-  }
+  private snackBar = inject(MatSnackBar);
 
   protected readonly userSig = signal<User>({
-    "firstName": "Markus",
-    "lastName": "Ederl",
+    "firstName": "",
+    "lastName": "",
     "age": "0",
-    "email": "markus.ederl@r-software.at",
-    "username": "MarkusEderl",
+    "email": "",
+    "username": "",
   });
 
+
+
   protected readonly userForm = form(this.userSig, (path) => {
-    apply(path.firstName, this._textSchema);
-    apply(path.lastName, this._textSchema);
-
-    email(path.email, { message: "Must be E-Mail" });
-
-    //OPTION 1
-    required(path.firstName, {
-      when: (ctx) => !!ctx.valueOf(path.lastName),
-      message: "Firstname is required if Lastname was defined"
-    });
-
-    required(path.lastName, {
-      when: (ctx) => !!ctx.valueOf(path.firstName),
-      message: "Lastname is required if Firstname is defined"
-    })
-
-    //OPTION 2
-    // applyWhenValue(path, (ctx) => !!ctx.firstName, (path) => {
-    //   required(path.lastName, { message: "Lastname is required." });
-    // });
-
-    // applyWhenValue(path, (ctx) => !!ctx.lastName, (path) => {
-    //   required(path.firstName, { message: "Firstname is required." });
-    // });
-
-    //OPTION 3
-    // applyWhen(path, (ctx) => !!ctx.valueOf(path.firstName), (path) => apply(path.lastName, this._requiredSchema));
-    // applyWhen(path, (ctx) => !!ctx.valueOf(path.lastName), (path) => apply(path.firstName, this._requiredSchema));
-
-
-    //Multi-Field; Tree Valdiators
-    this._validateFirstNameLastNameSame(path);
+    required(path.firstName, { message: "Firstname is required." }),
+      minLength(path.firstName, 3, { message: "Firstname must have at least 3 Characters." }),
+      maxLength(path.firstName, 30, { message: "Firstname must not have more than 30 Characters." })
   });
 
   protected async saveProposal() {
     await submit(this.userForm, async (form) => {
       try {
         const response = await firstValueFrom(this.userService.addUser(form().value()));
+        this.snackBar.open("User gespeichert", "OK");
       } catch (error) {
+        this.snackBar.open("User nicht gespeichert", "OK");
         return [{
           //field: this.userForm.firstName,
           kind: "server",
