555c3cdb4eb8903af5534f48559d576029460b17
Markus Ederl <markus.ederl@r-software.at>
2026-01-23 12:17:46 +0100

    autoCommit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { apply, applyWhen, applyWhenValue, email, FormField, form, MAX_LENGTH, maxLength, MIN_LENGTH, minLength, required, schema, Schema, SchemaPath, validateTree, applyEach } from '@angular/forms/signals';
+import { apply, applyWhenValue, email, FormField, form, maxLength, minLength, required, schema, Schema, applyEach } from '@angular/forms/signals';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { User } from '../data/users';
